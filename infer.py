0567d6c6a0fa0f2cb600b5cbb246ea575167f628
cc-visionary <lim.christopher.go@gmail.com>
2023-08-08 01:50:20 +0800

    Added YoLo v7 model for inference then refactored codes into functions

--- infer.py
+++ infer.py
@@ -3,7 +3,6 @@ from scipy import ndimage
 from skimage.measure import label, regionprops
 from flask_cors import CORS, cross_origin
 from dotenv import load_dotenv
-from torchvision.ops import box_iou
 
 import cv2
 import numpy as np
@@ -16,361 +15,74 @@ import io
 import boto3
 import os
 
+from utilities import infer_deeplab, infer_yolov7, infer_thresholding, create_overlay, preprocessing
+
 load_dotenv()
 
 application = flask.Flask(__name__)
 CORS(application)
 
-model = None
-CLASSES = {0: 'non-river', 1: 'river'}
-DEVICE = torch.device(
-    'cuda') if torch.cuda.is_available() else torch.device('cpu')
-MODEL_PATH = "./model/fine_tune_whole.pth"
+DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
 source_points = np.array([[210, 100], [430, 100], [120, 260], [520, 260]], dtype=np.float32)
 destination_points = np.array([[520, 400], [720, 400], [520, 560], [720, 560]], dtype=np.float32)
 M = cv2.getPerspectiveTransform(source_points, destination_points)
 
-def initalize():
-    global model
-    model = MSC(
-        base=DeepLabV2(
-            n_classes=len(CLASSES), n_blocks=[3, 4, 23, 3], atrous_rates=[6, 12, 18, 24]
-        ),
-        scales=[0.5, 0.75],
-    )
+"""
+Infers the Whole System
+1. performs river segmentation
+2. performs water hyacinth detection along with garbage detection,
+3. performs perspective transformation on the image
+4. calculates the percentage of river covere by river surface objects
+5. returns segmented, annotated, and warped images, along with percentage of river covered
+"""
+def infer_system(img, raw_img):
+    height, width, _ = raw_img.shape
+    segmented_img, river_mask = infer_deeplab(img, raw_img, DEVICE)
+    if(type(segmented_img) == np.ndarray):
+        g_small_coords, g_med_large_coords = infer_yolov7(segmented_img, DEVICE)
+        wh_small_coords, wh_med_large_coords = infer_thresholding(segmented_img)
 
-    state_dict = torch.load(
-        MODEL_PATH, map_location=lambda storage, loc: storage)
-    model.load_state_dict(state_dict)
-    model = torch.nn.DataParallel(model)
-    model.eval()
-    model.to(DEVICE)
-
-
-def white_mask(image):
-    r = np.zeros_like(image).astype(np.uint8)
-    g = np.zeros_like(image).astype(np.uint8)
-    b = np.zeros_like(image).astype(np.uint8)
-    r[image == 1], g[image == 1], b[image == 1] = [255, 255, 255]
-    mask = np.stack([r, g, b], axis=2)
-
-    return mask
-
-
-def preprocessing(image):
-    # Resize
-    # image = cv2.resize(image, dsize=(
-    #     640, 360), interpolation=cv2.INTER_LINEAR)
-    raw_image = image.astype(np.uint8)
-
-    # Subtract mean values
-    image = image.astype(np.float32)
-    image -= np.array(
-        [
-            float(104.008),
-            float(116.669),
-            float(122.675),
-        ]
-    )
-
-    # Convert to torch.Tensor and add "batch" axis
-    image = torch.from_numpy(image.transpose(2, 0, 1)).float().unsqueeze(0)
-
-    return image, raw_image
-
-
-_BATCH_NORM = nn.BatchNorm2d
-_BOTTLENECK_EXPANSION = 4
-
-
-class _ConvBnReLU(nn.Sequential):
-    """
-    Cascade of 2D convolution, batch norm, and ReLU.
-    """
-    BATCH_NORM = _BATCH_NORM
-
-    def __init__(
-        self, in_ch, out_ch, kernel_size, stride, padding, dilation, relu=True
-    ):
-        super(_ConvBnReLU, self).__init__()
-        self.add_module(
-            "conv",
-            nn.Conv2d(
-                in_ch, out_ch, kernel_size, stride, padding, dilation, bias=False
-            ),
-        )
-        self.add_module("bn", _BATCH_NORM(
-            out_ch, eps=1e-5, momentum=1 - 0.999))
-
-        if relu:
-            self.add_module("relu", nn.ReLU())
-
-
-class _Bottleneck(nn.Module):
-    """
-    Bottleneck block of MSRA ResNet.
-    """
-
-    def __init__(self, in_ch, out_ch, stride, dilation, downsample):
-        super(_Bottleneck, self).__init__()
-        mid_ch = out_ch // _BOTTLENECK_EXPANSION
-        self.reduce = _ConvBnReLU(in_ch, mid_ch, 1, stride, 0, 1, True)
-        self.conv3x3 = _ConvBnReLU(
-            mid_ch, mid_ch, 3, 1, dilation, dilation, True)
-        self.increase = _ConvBnReLU(mid_ch, out_ch, 1, 1, 0, 1, False)
-        self.shortcut = (
-            _ConvBnReLU(in_ch, out_ch, 1, stride, 0, 1, False)
-            if downsample
-            else nn.Identity()
-        )
-
-    def forward(self, x):
-        h = self.reduce(x)
-        h = self.conv3x3(h)
-        h = self.increase(h)
-        h += self.shortcut(x)
-        return F.relu(h)
-
-
-class _ResLayer(nn.Sequential):
-    """
-    Residual layer with multi grids
-    """
-
-    def __init__(self, n_layers, in_ch, out_ch, stride, dilation, multi_grids=None):
-        super(_ResLayer, self).__init__()
-
-        if multi_grids is None:
-            multi_grids = [1 for _ in range(n_layers)]
-        else:
-            assert n_layers == len(multi_grids)
-
-        # Downsampling is only in the first block
-        for i in range(n_layers):
-            self.add_module(
-                "block{}".format(i + 1),
-                _Bottleneck(
-                    in_ch=(in_ch if i == 0 else out_ch),
-                    out_ch=out_ch,
-                    stride=(stride if i == 0 else 1),
-                    dilation=dilation * multi_grids[i],
-                    downsample=(True if i == 0 else False),
-                ),
-            )
-
-
-class _Stem(nn.Sequential):
-    """
-    The 1st conv layer.
-    Note that the max pooling is different from both MSRA and FAIR ResNet.
-    """
-
-    def __init__(self, out_ch):
-        super(_Stem, self).__init__()
-        self.add_module("conv1", _ConvBnReLU(3, out_ch, 7, 2, 3, 1))
-        self.add_module("pool", nn.MaxPool2d(3, 2, 1, ceil_mode=True))
-
-
-class ResNet(nn.Sequential):
-    def __init__(self, n_classes, n_blocks):
-        super(ResNet, self).__init__()
-        ch = [64 * 2 ** p for p in range(6)]
-        self.add_module("layer1", _Stem(ch[0]))
-        self.add_module("layer2", _ResLayer(n_blocks[0], ch[0], ch[2], 1, 1))
-        self.add_module("layer3", _ResLayer(n_blocks[1], ch[2], ch[3], 2, 1))
-        self.add_module("layer4", _ResLayer(n_blocks[2], ch[3], ch[4], 2, 1))
-        self.add_module("layer5", _ResLayer(n_blocks[3], ch[4], ch[5], 2, 1))
-        self.add_module("pool5", nn.AdaptiveAvgPool2d(1))
-        self.add_module("flatten", nn.Flatten())
-        self.add_module("fc", nn.Linear(ch[5], n_classes))
-
-
-class MSC(nn.Module):
-    """
-    Multi-scale inputs
-    """
-
-    def __init__(self, base, scales=None):
-        super(MSC, self).__init__()
-        self.base = base
-        if scales:
-            self.scales = scales
-        else:
-            self.scales = [0.5, 0.75]
-
-    def forward(self, x):
-        # Original
-        logits = self.base(x)
-        _, _, H, W = logits.shape
-
-        def interp(l): return F.interpolate(
-            l, size=(H, W), mode="bilinear", align_corners=False
-        )
-
-        # Scaled
-        logits_pyramid = []
-        for p in self.scales:
-            h = F.interpolate(x, scale_factor=p,
-                              mode="bilinear", align_corners=False)
-            logits_pyramid.append(self.base(h))
-
-        # Pixel-wise max
-        logits_all = [logits] + [interp(l) for l in logits_pyramid]
-        logits_max = torch.max(torch.stack(logits_all), dim=0)[0]
-
-        if self.training:
-            return [logits] + logits_pyramid + [logits_max]
-        else:
-            return logits_max
-
-
-class _ASPP(nn.Module):
-    """
-    Atrous spatial pyramid pooling (ASPP)
-    """
-
-    def __init__(self, in_ch, out_ch, rates):
-        super(_ASPP, self).__init__()
-        for i, rate in enumerate(rates):
-            self.add_module(
-                "c{}".format(i),
-                nn.Conv2d(in_ch, out_ch, 3, 1, padding=rate,
-                          dilation=rate, bias=True),
-            )
-
-        for m in self.children():
-            nn.init.normal_(m.weight, mean=0, std=0.01)
-            nn.init.constant_(m.bias, 0)
-
-    def forward(self, x):
-        return sum([stage(x) for stage in self.children()])
-
-
-class DeepLabV2(nn.Sequential):
-    """
-    DeepLab v2: Dilated ResNet + ASPP
-    Output stride is fixed at 8
-    """
-
-    def __init__(self, n_classes, n_blocks, atrous_rates):
-        super(DeepLabV2, self).__init__()
-        ch = [64 * 2 ** p for p in range(6)]
-        self.add_module("layer1", _Stem(ch[0]))
-        self.add_module("layer2", _ResLayer(n_blocks[0], ch[0], ch[2], 1, 1))
-        self.add_module("layer3", _ResLayer(n_blocks[1], ch[2], ch[3], 2, 1))
-        self.add_module("layer4", _ResLayer(n_blocks[2], ch[3], ch[4], 1, 2))
-        self.add_module("layer5", _ResLayer(n_blocks[3], ch[4], ch[5], 1, 4))
-        self.add_module("aspp", _ASPP(ch[5], n_classes, atrous_rates))
-
-    def freeze_bn(self):
-        for m in self.modules():
-            if isinstance(m, _ConvBnReLU.BATCH_NORM):
-                m.eval()
-
-# ================ Create Overlay Mask ====================
-def create_overlay(mask):
-    r = np.zeros_like(mask).astype(np.uint8)
-    g = np.zeros_like(mask).astype(np.uint8)
-    b = np.zeros_like(mask).astype(np.uint8)
-    r[mask == 1], g[mask == 1], b[mask == 1] = [0, 255, 0]
-    
-    overlay = np.stack([r, g, b], axis=2)
-    
-    return overlay
-
-# ================ Segment River ====================
-def segment_river(image, raw_image):
-    _, _, H, W = image.shape
-
-    # Image -> Probability map
-    logits = model(image)
-    logits = F.interpolate(logits, size=(
-        H, W), mode="bilinear", align_corners=False)
-    probs = F.softmax(logits, dim=1)[0]
-    probs = probs.detach().cpu().numpy()
-
-    labelmap = np.argmax(probs, axis=0)
-
-    labelmap = (~labelmap.astype(bool)).astype(int)
-    labelmap = ndimage.binary_fill_holes(1 - labelmap).astype(int)
-    labelmap = ndimage.binary_fill_holes(1 - labelmap).astype(int)
-    labelmap = ndimage.binary_dilation(labelmap).astype(int)
-
-    total_river_pixels = (H * W) - cv2.countNonZero(labelmap)
-    # check if image is a river (river pixel > 40% of the image)
-    if (total_river_pixels < (H * W) * 0.40):
-        return [], []
-
-    w_mask = white_mask(labelmap)
-    raw_image = cv2.addWeighted(raw_image, 1, w_mask, 1, 0)
-    
-    labelmap = ndimage.binary_fill_holes(labelmap - 1)
-
-    return raw_image, labelmap.astype('uint8')
-
-# performs image processing techniques,
-# adds polygons to detected regions,
-# then performs perspective transformation on the image
-# ================ Locate Objects ====================
-def locate_objects(img, raw_img):
-    segmented, river_mask = segment_river(img, raw_img)
-    if(type(segmented) == np.ndarray):
-        hsv_img = cv2.cvtColor(segmented, cv2.COLOR_BGR2HSV)
-
-        mask = cv2.inRange(hsv_img, np.array(
-            [50, 100, 40]), np.array([90, 150, 160]))
-
-        # Noise removal using Morphological open operation
-        kernel = np.ones((3, 3), np.uint8)
-        dilate = cv2.dilate(mask, kernel, iterations=2)
-        # morphClose = cv2.morphologyEx(dilate, cv2.MORPH_CLOSE, kernel, iterations=1)
-        morphOpen = cv2.morphologyEx(dilate, cv2.MORPH_OPEN, kernel, iterations=1)
-
-        labels = regionprops(label(morphOpen))
-
+        predicted_obj_mask = np.zeros((height, width))
         detected_img = raw_img.copy()
-        detected_mask = np.zeros((360, 640))
 
-        small_coords = []
-        total_small_area = 0
-        medium_large_coords = []
-        total_medium_large_area = 0
-        for l in labels:
-            coords = np.array([[[b, a] for a, b in l.coords]])
-            if(l.area <= 32 * 32):
-                total_small_area += l.area
-                small_coords.append(coords[0])
-            else: # if area > 32*32
-                cv2.drawContours(detected_mask, coords, 0, 1, 1)
-                medium_large_coords.append(coords[0])
+        g_count = len(g_med_large_coords)
+        wh_count = len(wh_med_large_coords)
 
-        # warp detected
-        warped_mask = cv2.warpPerspective(detected_mask, M, (1240, 610), flags=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT, borderValue=0)
-        warped_img = cv2.warpPerspective(detected_img, M, (1240, 610), flags=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT, borderValue=0)    
-        total_warped_medium_large_area = cv2.countNonZero(warped_mask)
+        print(g_count, wh_count)
+
+        # if small_objects collectively > 32 * 32
+        total_small_area = sum([area for _, area in [*g_small_coords, *wh_small_coords]])
+        if(total_small_area > 32 * 32):
+            g_count += len(g_small_coords)
+            wh_count += len(wh_small_coords)
+            for small_coords in [*g_small_coords, *wh_small_coords]:
+                coord, _ = small_coords
+                cv2.drawContours(predicted_obj_mask, [coord], 0, 1, -1)
+
+        for med_large_coords in [*g_med_large_coords, *wh_med_large_coords]:
+            coord, _ = med_large_coords
+            cv2.drawContours(predicted_obj_mask, [coord], 0, 1, -1)
+
+        total_obj_area = cv2.countNonZero(predicted_obj_mask)
+
+        # warp detected 
+        warped_obj_mask = cv2.warpPerspective(predicted_obj_mask, M, (1240, 610), flags=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT, borderValue=0)
+        warped_img = cv2.warpPerspective(raw_img, M, (1240, 610), flags=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT, borderValue=0)    
+        total_warped_obj_area = cv2.countNonZero(warped_obj_mask)
             
         # warp river mask
         warped_river = cv2.warpPerspective(river_mask.astype('uint8'), M, (1240, 610), flags=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT, borderValue=0)
         transformed_river_area = cv2.countNonZero(warped_river)
 
-        # only label the small areas if total area > 32 * 32
-        if(total_small_area > 32 * 32):
-            cv2.drawContours(detected_mask, small_coords, -1, 1, 1)
-            for coords in small_coords:
-                warped_coords = cv2.perspectiveTransform(np.array([coords], dtype=np.float32), M)
-                cv2.drawContours(warped_mask, warped_coords.astype(int), -1, 1, 1)
-
-        detected_img = cv2.addWeighted(detected_img, 1, create_overlay(detected_mask), 0.25, 0)
-        warped_img = cv2.addWeighted(warped_img, 1, create_overlay(warped_mask), 0.25, 0)
+        detected_img = cv2.addWeighted(detected_img, 1, create_overlay(predicted_obj_mask), 0.25, 0)
+        warped_img = cv2.addWeighted(warped_img, 1, create_overlay(warped_obj_mask), 0.25, 0)
         
-        total_area = cv2.countNonZero(detected_mask)
-        transformed_total_area = cv2.countNonZero(warped_mask)
-        river_covered = min(total_area / cv2.countNonZero(river_mask) * 100, 100)
-        transformed_river_covered = min(transformed_total_area / transformed_river_area * 100, 100)
+        river_covered = min(total_obj_area / cv2.countNonZero(river_mask) * 100, 100)
+        transformed_river_covered = min(total_warped_obj_area / transformed_river_area * 100, 100)
 
         # returns bboxed image, warped bbox image, actual river covered, transformed river covered
-        return segmented, detected_img, warped_img, river_covered, transformed_river_covered
+        return segmented_img, detected_img, warped_img, river_covered, transformed_river_covered
     else:
         return None, None, None, None, None
 
@@ -403,8 +115,8 @@ def predict():
             img = np.array(img)
             image, raw_image = preprocessing(img)
 
-            # locate_objects while retrieving river covered
-            segmented, detected_img, warped_img, actual_river_covered, transformed_river_covered = locate_objects(image, raw_image)
+            # locate objects while retrieving river covered
+            segmented, detected_img, warped_img, actual_river_covered, transformed_river_covered = infer_system(image, raw_image)
 
             if (type(detected_img) == np.ndarray):
                 # Upload the file
@@ -446,5 +158,4 @@ def predict():
 if __name__ == "__main__":
     print("Loading inference server")
     print("Utilizing %s for computing" % DEVICE)
-    initalize()
     application.run(host='localhost', port='8000')
---
+++ utilities.py
@@ -0,0 +1,256 @@
+# Import Libraries
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import sys
+import cv2
+import numpy as np
+
+from shapely import geometry
+from scipy import ndimage
+from skimage.measure import label, regionprops
+
+sys.path.insert(0, "./yolov7")
+from yolov7.models.experimental import Ensemble
+from yolov7.models.common import Conv
+from yolov7.utils.datasets import letterbox
+from yolov7.utils.general import check_img_size, non_max_suppression, scale_coords
+
+sys.path.insert(0, "./deeplab")
+from deeplab.deeplabv2 import DeepLabV2
+from deeplab.msc import MSC
+
+# ================== General Utilities ==================
+"""
+Generates a transparent RGB mask based on a binary mask
+"""
+def create_overlay(mask):
+    r = np.zeros_like(mask).astype(np.uint8)
+    g = np.zeros_like(mask).astype(np.uint8)
+    b = np.zeros_like(mask).astype(np.uint8)
+    r[mask == 1], g[mask == 1], b[mask == 1] = [0, 255, 0]
+    
+    overlay = np.stack([r, g, b], axis=2)
+    
+    return overlay
+
+"""
+Generates a white RGB mask based on a binary mask 
+"""
+def white_mask(image):
+    r = np.zeros_like(image).astype(np.uint8)
+    g = np.zeros_like(image).astype(np.uint8)
+    b = np.zeros_like(image).astype(np.uint8)
+    r[image == 1], g[image == 1], b[image == 1] = [255, 255, 255]
+    mask = np.stack([r, g, b], axis=2)
+
+    return mask
+
+"""
+Does the following pre-processing steps for the image:
+1. resize the image to a width of 640 while preserving the overall aspect ratio
+2. normalize by substracting the original RGB values
+3. convert the image to torch format for passing it to deeplab model
+"""
+def preprocessing(image):
+    height, width, _ = image.shape
+
+    # Resize
+    # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    scale = width / 640
+    image = cv2.resize(image, dsize=(round(width / scale), round(height / scale)), interpolation=cv2.INTER_LINEAR)
+    raw_image = image.astype(np.uint8)
+
+    # Subtract mean values
+    image = image.astype(np.float32)
+    image -= np.array(
+        [
+            float(104.008),
+            float(116.669),
+            float(122.675),
+        ]
+    )
+
+    # Convert to torch.Tensor and add "batch" axis
+    image = torch.from_numpy(image.transpose(2, 0, 1)).float().unsqueeze(0)
+
+    return image, raw_image
+
+# ================== Image Processing Techniques / Water Hyacinth Detection ==================
+def infer_thresholding(segmented_image):
+    hsv_img = cv2.cvtColor(segmented_image, cv2.COLOR_BGR2HSV)
+
+    mask = cv2.inRange(hsv_img, np.array([50, 100, 40]), np.array([90, 150, 160]))
+
+    # Noise removal using Morphological open operation
+    kernel = np.ones((3, 3), np.uint8)
+    dilate = cv2.dilate(mask, kernel, iterations=3)
+    morphClose = cv2.morphologyEx(dilate, cv2.MORPH_CLOSE, kernel, iterations=5)
+    morphOpen = cv2.morphologyEx(morphClose, cv2.MORPH_OPEN, kernel, iterations=2)
+
+    labels = regionprops(label(morphOpen))
+
+    # retrieve small objects and medium + large objects
+    small_coords = []
+    medium_large_coords = []
+    for l in labels:
+        coords = np.array([[[b, a] for a, b in l.coords]])
+        if(l.area <= 32 * 32):
+            small_coords.append((coords[0], l.area))
+        else: # if area > 32*32
+            medium_large_coords.append((coords[0], l.area))
+
+    return small_coords, medium_large_coords
+    
+
+# ================== YOLO v7 Utilities / Garbage Detection ==================
+""" 
+Load the YoLo v7 model from the trained weights
+"""
+def load_yolov7(device):
+    model = Ensemble()
+
+    weight = torch.load('./model/yolo.pt',
+                        map_location=device)  # load FP32 model
+    model.append(weight['ema' if weight.get('ema')
+                 else 'model'].float().fuse().eval())
+
+    # Compatibility updates
+    for m in model.modules():
+        if type(m) in [nn.Hardswish, nn.LeakyReLU, nn.ReLU, nn.ReLU6, nn.SiLU]:
+            m.inplace = True  # pytorch 1.7.0 compatibility
+        elif type(m) is nn.Upsample:
+            m.recompute_scale_factor = None  # torch 1.11.0 compatibility
+        elif type(m) is Conv:
+            m._non_persistent_buffers_set = set()  # pytorch 1.6.0 compatibility
+
+    if len(model) == 1:
+        return model[-1]  # return model
+    else:
+        print('Ensemble created with %s\n' % weights)
+        for k in ['names', 'stride']:
+            setattr(model, k, getattr(model[-1], k))
+        return model  # return ensemble
+
+""" 
+Infer the YoLo v7 model by passing an input image 
+"""
+def infer_yolov7(segmented_img, device):
+    model = load_yolov7(device)
+    stride = int(model.stride.max())  # model stride
+    imgsz = check_img_size(640, s=stride)  # check img_size
+
+    # Run inference
+    if device != 'cpu':
+        model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(
+            next(model.parameters())))  # run once
+    old_img_w = old_img_h = imgsz
+    old_img_b = 1
+
+    # Get names and colors
+    # names = model.module.names if hasattr(model, 'module') else model.names
+
+    img = letterbox(segmented_img, imgsz, stride)[0]
+    
+    # Convert
+    img = img.transpose(2, 0, 1)  # to 3xheightxwidth
+    img = np.ascontiguousarray(img)
+
+    img = torch.from_numpy(img).to(device)
+    img = img / 255.0  # 0 - 255 to 0.0 - 1.0
+    if img.ndimension() == 3:
+        img = img.unsqueeze(0)
+
+    # Warmup
+    if device != 'cpu' and (old_img_b != img.shape[0] or old_img_h != img.shape[2] or old_img_w != img.shape[3]):
+        old_img_b = img.shape[0]
+        old_img_h = img.shape[2]
+        old_img_w = img.shape[3]
+        for i in range(3):
+            model(img)[0]
+
+    # Inference
+    with torch.no_grad():   # Calculating gradients would cause a GPU memory leak
+        pred = model(img)[0]
+
+    # Apply NMS
+    pred = non_max_suppression(pred)
+
+    bboxes = []
+    # scores = []
+    # Process detections
+    for det in pred:  # detections per image
+        if len(det):
+            # Rescale boxes from img_size to im0 size
+            det[:, :4] = scale_coords(
+                img.shape[2:], det[:, :4], segmented_img.shape).round()
+
+            for *xyxy, conf, _ in reversed(det):
+                bboxes.append(torch.tensor(xyxy).view(1, 4)[0].numpy())
+                # scores.append(conf.cpu().numpy())
+
+    small_coords = []
+    medium_large_coords = []
+
+    for x1, y1, x2, y2 in bboxes:
+        width = x2 - x1
+        height = y2 - y1
+
+        if(height * width <=- 32 * 32):
+            small_coords.append((np.array([[[x1, y1]], [[x2, y1]], [[x2, y2]], [[x1, y2]]], np.int32), height * width))
+        else:
+            medium_large_coords.append((np.array([[[x1, y1]], [[x2, y1]], [[x2, y2]], [[x1, y2]]], np.int32), height * width))
+
+    return small_coords, medium_large_coords
+
+# ================== DeepLab Utilities / River Segmentation ==================
+CLASSES = {0: 'non-river', 1: 'river'}
+
+"""
+Loads the DeepLab model using the fine-tuned weights
+"""
+def load_deeplab(device):
+    model = MSC(
+        base=DeepLabV2(
+            n_classes=len(CLASSES), n_blocks=[3, 4, 23, 3], atrous_rates=[6, 12, 18, 24]
+        ),
+        scales=[0.5, 0.75],
+    )
+
+    state_dict = torch.load("./model/deeplab.pth", map_location=lambda storage, loc: storage)
+    model.load_state_dict(state_dict)
+    model = torch.nn.DataParallel(model)
+    model.eval()
+    model.to(device)
+
+    return model
+
+"""
+Infers the DeepLab v2 model
+"""
+def infer_deeplab(image, raw_image, device):
+    model = load_deeplab(device)
+
+    H, W, _ = raw_image.shape
+
+    # Image -> Probability map
+    logits = model(image)
+    logits = F.interpolate(logits, size=(
+        H, W), mode="bilinear", align_corners=False)
+    probs = F.softmax(logits, dim=1)[0]
+    probs = probs.detach().cpu().numpy()
+
+    labelmap = np.argmax(probs, axis=0)
+    labelmap = (~labelmap.astype(bool)).astype(int)
+
+    total_river_pixels = (H * W) - cv2.countNonZero(labelmap)
+    # check if image is a river (river pixel > 40% of the image)
+    # if (total_river_pixels < (H * W) * 0.40):
+    #     return [], []
+
+    w_mask = white_mask(labelmap)
+    raw_image = cv2.addWeighted(raw_image, 1, w_mask, 1, 0)
+    
+    labelmap = ndimage.binary_fill_holes(labelmap - 1)
+
+    return raw_image, labelmap.astype('uint8')
